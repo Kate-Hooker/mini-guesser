c727f1b5eb6b250fe2d205f8184cae3aa574a7ca
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-28 12:49:36 +1300

    issue with prompt-sync depedancies. Cant be bothered debugging dependancies. Project sent to graveyard

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 const prompt = require('prompt-sync')
 
 const target = 10 + Math.round(Math.random() * 90)
-console.log(target)
+
+let guesses = 0
+
+while (true) {
+  const guess = Number(prompt('Guess a number between 1 and 90: '))
+  //convert guess from string to number
+  if (guess > target) {
+    console.log('Too high')
+  } else if (guess < target) {
+    console.log('Too low')
+  } else {
+    console.log('Correct')
+  }
+  break
+}
